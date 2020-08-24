3bf3a307c668ff6d64cad66bedaeda30246bde72
clocks-in-a-cooler <34520271+Clocks-in-a-cooler@users.noreply.github.com>
2020-08-23 22:54:46 -0400

    added error pane, plus error sending
    uncompressed size: 5.7 kB

---
+++ errors.js
@@ -0,0 +1,11 @@
+var error_score = 0;
+
+function send_error(message, code) {
+    if (!code) {
+        code = 400;
+    }
+    document.getElementById("errors").appendChild(create_element("error",
+        `error ${ code }: ${ message }`));
+    
+    error_score += code;
+}
